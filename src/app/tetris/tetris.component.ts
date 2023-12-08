54027279bf491f8ee56e59e28d67a640bf38737e
Benjamin <bpeyre@univ-pau.fr>
2023-12-08 06:39:53 +0100

    Ajout du tetris

--- src/app/tetris/tetris.component.ts
+++ src/app/tetris/tetris.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tetris',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./tetris.component.scss']
 })
 export class TetrisComponent {
-  
-}
+  board: boolean[][] = [];
+  ngOnInit() {
+    this.initBoard();
+  }
+
+  private initBoard(): void {
+    for (let i = 0; i < 20; i++) {
+      this.board.push(Array(10).fill(false));
+    }
+  }
+} 
